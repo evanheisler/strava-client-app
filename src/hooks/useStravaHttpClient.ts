222242eeb6060dd36ed9d8b23b68e2620e496d8c
Evan Heisler <evanheisler@gmail.com>
2023-06-03 16:54:31 -0700

    Configure Auth Context

---
+++ src/hooks/useStravaHttpClient.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { useContext } from 'react';
+import {
+  AuthContext,
+  TAuthConfig,
+  TRefreshTokenExpiredEvent,
+} from 'react-oauth2-code-pkce';
+
+export const stravaOauth2Config: TAuthConfig = {
+  clientId: import.meta.env.VITE_STRAVA_CLIENT_ID,
+  authorizationEndpoint: 'https://www.strava.com/oauth/authorize',
+  tokenEndpoint: 'https://www.strava.com/oauth/token',
+  redirectUri: 'http://localhost:5173/',
+  scope: 'read_all',
+  onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) =>
+    window.confirm(
+      'Session expired. Refresh page to continue using the site?'
+    ) && event.login(),
+  extraAuthParameters: {
+    responseType: 'code',
+  },
+  extraTokenParameters: {
+    client_secret: import.meta.env.VITE_STRAVA_CLIENT_SECRET,
+  },
+  autoLogin: false,
+  decodeToken: false,
+};
+
+const stravaApi = axios.create({
+  baseURL: 'https://www.strava.com/api/v3',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+export const useStravaHttpClient = () => {
+  const { token } = useContext(AuthContext);
+
+  stravaApi.interceptors.request.use(
+    async (config) => {
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  return {
+    stravaApi,
+  };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AuthProvider } from 'react-oauth2-code-pkce';
+import App from './App.tsx';
+import { stravaOauth2Config } from './hooks/useStravaHttpClient.ts';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider authConfig={stravaOauth2Config}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
