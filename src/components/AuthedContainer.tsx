7fce630558a74f81e3b766379310093e7859ef6a
Evan Heisler <evanheisler@gmail.com>
2023-06-03 17:31:43 -0700

    Init Authed route; remove boilerplate

---
+++ src/components/AuthedContainer.tsx
@@ -0,0 +1,23 @@
+import { AppBar, Box, Button, Container, Toolbar } from '@mui/material';
+import { useContext } from 'react';
+import { AuthContext } from 'react-oauth2-code-pkce';
+
+export const AuthedContainer = () => {
+  const { logOut, idTokenData } = useContext(AuthContext);
+
+  return (
+    <AppBar position="static" color="transparent">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box marginLeft="auto">
+            <Button
+              variant="outlined"
+              onClick={() => logOut('rememberThis', idTokenData?.tid)}>
+              Log Out
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from 'react-oauth2-code-pkce';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.tsx';
 import { stravaOauth2Config } from './hooks/useStravaHttpClient.ts';
-import './index.css';
+import { StyledEngineProvider } from '@mui/material';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider authConfig={stravaOauth2Config}>
-      <App />
-    </AuthProvider>
+    <StyledEngineProvider injectFirst>
+      <AuthProvider authConfig={stravaOauth2Config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </AuthProvider>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
